6362b80f2cb6cb7df5fee0ca14b81530fc3ed49a
poornesh-pH <49716324+poornesh-pH@users.noreply.github.com>
2022-09-16 21:03:06 +0530

    1 file modified, 2 added

---
+++ src/moo/filter-lines.js
@@ -0,0 +1,3 @@
+//filter lines based on the mtn===8484330116
+// lineLevelPlans -> lines should return with single object with the  specified mdn
+//all other property should remain same other than lines
